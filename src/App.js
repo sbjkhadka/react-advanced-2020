769c93241f803455673bb3c85ba41c31dbe501cb
Subarna Bijaya Khadka <khadka.subarnabijaya@gmail.com>
2022-03-08 23:38:29 -0500

    short circuiting and unary operator

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ import UseEffectBasics from "./tutorial/2-useEffect/setup/1-useEffect-basics";
 import UseEffectCleanup from "./tutorial/2-useEffect/setup/2-useEffect-cleanup";
 import UseEffectFetchData from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
 import MultipleReturns from "./tutorial/3-conditional-rendering/setup/1-multiple-returns";
+import ShortCircuit from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
+import ShowHide from "./tutorial/3-conditional-rendering/setup/3-show-hide";
+
 function App() {
   return (
     <div className="container">
@@ -22,7 +25,7 @@ function App() {
       {/* <UseEffectBasics /> */}
       {/* <UseEffectCleanup /> */}
       {/* <UseEffectFetchData /> */}
-      <MultipleReturns />
+      <ShowHide />
     </div>
   );
 }
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+      <h1>{text || "john Doe"}</h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle error
+      </button>
+      {/* {text && <h1>2hello World</h1>}
+      {!text && <h1>3hello World</h1>} */}
+      {isError && <h1>Error...</h1>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>There is no error</h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
